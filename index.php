1eb0776eeeef51043c78315367752e2e704a20c9
Lærke Nielsen <laenie01@easv365.dk>
2024-09-23 15:05:22 +0200

    Added my own library

--- index.php
+++ index.php
@@ -6,8 +6,64 @@
     <title>Document</title>
     <link rel="stylesheet" href="style/style.css">
     <link rel="stylesheet" href="https://use.typekit.net/arj0iay.css">
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body>
+
+
+<!-- news -->
+<section>
+    <div>
+        <h2>NEWS</h2>
+    </div>
+    
+    <!-- loop with news -->
+    <div>
+        <div>
+            <img src="" alt="Image of news">
+            <h4></h4>
+            <button class="btn">See more</button>
+        </div>
+    </div>
+
+</section>
+
+
+<!-- upcoming movies -->
+<section>
+    <div>
+        <h2>Upcoming movies</h2>
+    </div>
+    
+    <!-- loop with movies -->
+    <div>
+        <div>
+            <img src="" alt="Image of movie">
+            <h4></h4>
+            <button class="btn">Get tickets</button>
+        </div>
+    </div>
+
+</section>
+
+
+<!-- about ghiblifilms -->
+<section>
+    <div class="about-ghiblifilms">
+        <div>
+            <h2 class="primary-color">About Ghiblifilms</h2>
+        </div>
+
+        <div>
+            <p class="primary-color">Welcome to Ghiblifilms, Copenhagen's enchanting cinema dedicated entirely to the magical world of Studio Ghibli. 
+                Nestled in the heart of the city, our cinema is a haven for Ghibli fans, where the charm and wonder of 
+                Hayao Miyazaki's timeless creations come to life. Our screenings exclusively feature Studio Ghibli classics, 
+                offering a truly immersive experience where the magic of the movies extends beyond the screen. </p>
+        </div>
+        
+    </div>
+</section>
+
     
 </body>
 </html>
