ac92fe0f85196aa02437a32dd2d437e964750d98
Lærke Nielsen <laenie01@easv365.dk>
2024-09-23 17:15:08 +0200

    Movie page styling set up

--- pages/moviePage.php
+++ pages/moviePage.php
@@ -1,16 +1,153 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Movie Page</title>
+    <link rel="stylesheet" href="../../style/style.css">
+    <link rel="stylesheet" href="../../style/library.css">
+    <link rel="stylesheet" href="../../style/responsive.css">
+    <link rel="stylesheet" href="https://use.typekit.net/arj0iay.css">
+</head>
+<body>
 
+<!-- movie overall info -->
 <section>
+    <div class="flex gap-10">
+        <!-- img of movie -->
+        <div class="h-full-vh">
+            <img src="../../img/chihiro.jpg" alt="Image of movie" class="h-90-vh">
+        </div>
 
+        <!-- info -->
+        <div class="pt-20 pr-4 w-half">   
+            <!-- title and description -->
+            <div class="pb-12">
+                <h1 class="pb-4">Ponyo</h1>
+                <p class="pb-8">IT ENDS WITH US, the first Colleen Hoover novel adapted for the big screen, tells the compelling story of Lily Bloom (Blake Lively), a woman who overcomes a traumatic childhood to embark on a new life in Boston and chase a lifelong dream of opening her own business. A chance meeting with charming neurosurgeon Ryle Kincaid (Justin Baldoni) sparks an intense connection, but as the two fall deeply in love, Lily begins to see sides of Ryle that remind her of her parents’ relationship. When Lily’s first love, Atlas Corrigan (Brandon Sklenar), suddenly reenters her life, her relationship with Ryle is upended, and Lily realizes she must learn to rely on her own strength to make an impossible choice for her future.</p>
+                <button class="btn">See times</button>
+            </div>
+           
+            <!-- key info -->
+            <div class="flex flex-col gap-6">
+                <!-- duration -->
+                <div>
+                    <h4 class="text-sm">Duration</h4>
+                    <p>1 HR 10 MIN.</p>
+                </div>
+
+                <!-- release date -->
+                <div>
+                    <h4 class="text-sm">Release date</h4>
+                    <p>AUG 9, 2024</p>
+                </div>
+
+                <!-- genre -->
+                <div>
+                    <p>DRAMA, ROMANCE</p>
+                </div>
+            </div>
+        </div>
+    </div>
 </section>
-
 
 <!-- cast -->
-<section>
+<section class="pt-24 pb-24">
+    <!-- voice actors -->
+    <div class="flex justify-between ten-percent pb-24">
+        <div class="flex">
+            <h3>VOICE ACTORS</h3>
+        </div>
 
+        <div class="flex flex-col w-half">
+            <p class="primary-font secondary-color text-lg">hi world</p>
+            <p class="primary-font secondary-color text-lg">hi world</p>
+            <p class="primary-font secondary-color text-lg">hi world</p>
+            <p class="primary-font secondary-color text-lg">hi world</p>
+            <p class="primary-font secondary-color text-lg">hi world</p>
+            <p class="primary-font secondary-color text-lg">hi world</p>
+            <p class="primary-font secondary-color text-lg">hi world</p>
+        </div>
+    </div>
+
+
+    <!-- production team -->
+    <div class="flex justify-between ten-percent pb-12">
+        <div class="flex">
+            <h3>PRODUCTION TEAM</h3>
+        </div>
+
+        <div class="flex flex-col w-half">
+            <div class="flex justify-between gap-6">
+                <!-- role -->
+                <div class="flex-1 text-right">
+                    <p class="primary-font secondary-color text-lg">PLANNING AND SCRIPT</p>
+                </div>
+
+                <!-- name -->
+                <div class="flex-1">
+                    <p class="secondary-color text-lg">HAYAO MIYAZAKI</p>
+                </div>
+            </div>
+
+            <div class="flex justify-between gap-6">
+                <!-- role -->
+                <div class="flex-1 text-right">
+                    <p class="primary-font secondary-color text-lg">PgweftwefweNNING AND SCRIPT</p>
+                </div>
+
+                <!-- name -->
+                <div class="flex-1">
+                    <p class="secondary-color text-lg">HAYAO MIfewfwefewfwYAZAKI</p>
+                </div>
+            </div>
+
+            <div class="flex justify-between gap-6">
+                <!-- role -->
+                <div class="flex-1 text-right">
+                    <p class="primary-font secondary-color text-lg">PLAN AND SCRIPT</p>
+                </div>
+
+                <!-- name -->
+                <div class="flex-1">
+                    <p class="secondary-color text-lg">HAYAO MIYAI</p>
+                </div>
+            </div>
+        </div>
+    </div>
 </section>
 
 
-<!-- book tickets -->
-<section>
+<!-- book slots -->
+<section class="pb-24">
+    <div class="flex justify-between ten-percent pb-12">
+        <!-- adress -->
+        <div class="flex">
+            <h3>FIELD'S SHOPPING MALL<br>ARNE JACOBSENS ALLÉ 12,<br>2300 COPENHAGEN</h3>
 
+        </div>
+
+        <div class="flex flex-col w-half gap-4">
+            <div class="booking-slot">
+                <h4>MON 17/9 AT 19.30</h4>
+                <p>BIO 1</p>
+                <p>2D</p>
+            </div>
+
+            <div class="booking-slot">
+                <h4>MON 17/9 AT 19.30</h4>
+                <p>BIO 1</p>
+                <p>2D</p>
+            </div>
+
+            <div class="booking-slot">
+                <h4>MON 17/9 AT 19.30</h4>
+                <p>BIO 1</p>
+                <p>2D</p>
+            </div>
+        </div>
+    </div>
 </section>
+
+</body>
+</html>
